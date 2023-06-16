e3e2d93c78f7d8d93808e6a7dacd9a059cea7219
jatin <jatinchowdhury18@gmail.com>
2023-06-16 00:18:32 -0700

    Fixing run_tests script

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+echo "Generating C++ code..."
+python3 generate_ndk_cpp.py test_configs/cry_baby_ndk_config.json
+mv CryBabyNDK.* tests/cry_baby_test
+
+echo "Configuring tests..."
 cmake -Bbuild-tests -Stests
 
 echo "Running Cry Baby test..."
-python3 generate_ndk_cpp.py test_configs/cry_baby_ndk_config.json
-mv CryBabyNDK.* tests/cry_baby_test
 cmake --build build-tests --target cry_baby_test --parallel
 ./build-tests/cry_baby_test/cry_baby_test
